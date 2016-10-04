c8b67811ada4b5e1fd25d5f61717088c07bedc37
Muhamad Visat Sutarno <mvisat@gmail.com>
2016-10-04 17:58:55 +0700

    fix psnr calculation

--- util.py
+++ util.py
@@ -3,15 +3,16 @@ from PIL import Image
 
 def psnr(img_a, img_b):
     if img_a.size != img_b.size:
-        return None
+        raise Exception("image size must be equal")
 
-    return 20 * math.log(256 / math.sqrt(mse(img_a, img_b)), 10)
+    rms = math.sqrt(mse(img_a, img_b))
+    return 20 * math.log10(255 / rms)
 
 def mse(img_a, img_b):
-    img_int_a = img_int(img_a)
-    img_int_b = img_int(img_b)
+    img_int_a = img_int(img_a.copy().convert("RGB"))
+    img_int_b = img_int(img_b.copy().convert("RGB"))
     tmp = sum((sum(a)-sum(b))**2 for a, b in zip(img_int_a, img_int_b))
-    return tmp / img_a.width / img_a.height
+    return tmp / img_a.width / img_a.height / 3
 
 def img_int(img):
     px = img.load()
